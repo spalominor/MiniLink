ce209fc7ddaa81b93e59933b3c0f5db05fac447a
spalominor <spalominor@unal.edu.co>
2024-05-03 23:11:11 -0500

    Crear tablas

--- minilink/db/config.ts
+++ minilink/db/config.ts
@@ -1,6 +1,26 @@
-import { defineDb } from 'astro:db';
+import { column, defineDb, defineTable } from 'astro:db';
+
+// Definir la tabla User para almacenar los usuarios
+const User = defineTable({
+  columns: {
+    id: column.number({ primaryKey: true}),
+    email: column.text(),
+    name: column.text()
+  }
+});
+
+// Definir la tabla ShortenedUrl para almacenar las URL acortadas
+const ShortenedUrl = defineTable({
+  columns: {
+    userId: column.number({ references: () => User.columns.id }),
+    url: column.text(),
+    code: column.text()
+  }
+});
 
 // https://astro.build/db/config
 export default defineDb({
-  tables: {}
+  tables: {
+
+  }
 });
